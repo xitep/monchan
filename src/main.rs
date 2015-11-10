8d97aa899ed6a15e14e0474a90e70633e73be33d
Novotnik, Petr <petr.novotnik@firma.seznam.cz>
2015-11-10 08:10:34 +0100

    #! Add produce/consumer integration test

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,11 @@ extern crate byteorder;
 extern crate stopwatch;
 
 use std::cmp;
+use std::collections::HashMap;
 use std::env;
 use std::iter::repeat;
-use kafka::client::KafkaClient;
+
+use kafka::client::{KafkaClient, Compression};
 use stopwatch::Stopwatch;
 
 fn main() {
@@ -25,25 +27,15 @@ fn main() {
 
     for cmd in cmds {
         let cmd: &str = &cmd;
-        match cmd {
-            "offsets" => {
-                if let Err(e) = print_offsets(&cfg) {
-                    println!("error: {:?}", e)
-                }
-            }
-            "produce" => {
-                if let Err(e) = produce_data(&cfg) {
-                    println!("error: {:?}", e);
-                }
-            }
-            "consume" => {
-                if let Err(e) = consume_data(&cfg) {
-                    println!("error: {:?}", e);
-                }
-            }
-            _ => {
-                println!("unknown command: {}", cmd);
-            }
+        let res = match cmd {
+            "dump-offsets" => print_offsets(&cfg),
+            "produce" => produce_data(&cfg),
+            "consume" => consume_data(&cfg),
+            "test-produce-consume-integration" => test_produce_consume_integration(&cfg),
+            _ => Err(Error::Other(format!("unknown command: {}", cmd))),
+        };
+        if let Err(e) = res {
+            println!("Error: {:?}", e);
         }
     }
 }
@@ -55,6 +47,8 @@ struct Config {
 
     produce_msg_per_topic: u32,
     produce_bytes_per_msg: u32,
+
+    compression: Compression,
 }
 
 impl Config {
@@ -65,8 +59,9 @@ impl Config {
         opts.optflag("h", "help", "Print this help screen");
         opts.optopt("", "brokers", "Specify brokers (comma separated)", "HOSTS");
         opts.optopt("", "topics", "Specify topics (comma separated)", "TOPICS");
-        opts.optopt("", "produce-msg-per-topic", "Produce N messages per topic", "N");
+        opts.optopt("", "produce-msgs-per-topic", "Produce N messages per topic", "N");
         opts.optopt("", "produce-bytes-per-msg", "Produce N bytes per message", "N");
+        opts.optopt("", "compression", "Set compression type [NONE, GZIP, SNAPPY]", "TYPE");
         let matches = match opts.parse(&args[1..]) {
             Ok(m) => m,
             Err(e) => return Err(e.to_string()),
@@ -85,7 +80,7 @@ impl Config {
                 .map(|s| s.split(',').map(|s| s.trim().to_owned()).collect())
                 .unwrap_or_else(|| vec![]),
             produce_msg_per_topic:
-            try!(matches.opt_str("produce-msg-per-topic")
+            try!(matches.opt_str("produce-msgs-per-topic")
                  .unwrap_or_else(|| "100000".to_owned())
                  .parse::<u32>()
                  .map_err(|e| format!("not a number: {}", e))),
@@ -94,6 +89,15 @@ impl Config {
                  .unwrap_or_else(|| "10".to_owned())
                  .parse::<u32>()
                  .map_err(|e| format!("not a number: {}", e))),
+            compression: {
+                let s = matches.opt_str("compression").unwrap_or_else(|| "NONE".to_owned());
+                match s.trim() {
+                    "none" | "NONE" => Compression::NONE,
+                    "gzip" | "GZIP" => Compression::GZIP,
+                    "snappy" | "SNAPPY" => Compression::SNAPPY,
+                    _ => return Err(format!("Unknown compression type: {}", s)),
+                }
+            },
         };
         let cmds = if matches.free.is_empty() {
             vec!["offsets".to_owned()]
@@ -116,11 +120,16 @@ impl Config {
 
 #[derive(Debug)]
 enum Error {
-    Kafka(kafka::error::Error)
+    Kafka(kafka::error::Error),
+    Other(String),
 }
 
 impl From<kafka::error::Error> for Error {
     fn from(e: kafka::error::Error) -> Self { Error::Kafka(e) }
+}
+
+impl<'a> From<&'a str> for Error {
+    fn from(s: &'a str) -> Self { Error::Other(s.to_owned()) }
 }
 
 fn print_offsets(cfg: &Config) -> Result<(), Error> {
@@ -254,5 +263,95 @@ fn consume_data(cfg: &Config) -> Result<(), Error> {
                topic, total, n_errors, n_bytes, elapsed_ms,
                (1000 * total) as f64 / elapsed_ms as f64);
     }
+    Ok(())
+}
+
+/// Produces messages to a topic and reads them back. Verifying none
+/// has been lost. Assumes no concurrent producers to the target
+/// topic.
+fn test_produce_consume_integration(cfg: &Config) -> Result<(), Error> {
+
+    fn do_test(client: &mut KafkaClient, topics: &[String], msg_per_topic: usize, sent_msg: &[u8]) -> Result<(), Error> {
+        // ~ the data set will be sending
+        let msgs = {
+            let mut msgs = Vec::with_capacity(topics.len() * msg_per_topic as usize);
+            for topic in topics {
+                for _ in 0..msg_per_topic {
+                    msgs.push(kafka::utils::ProduceMessage { topic: topic.to_owned(), message: sent_msg.to_owned() });
+                }
+            }
+            msgs
+        };
+
+        // ~ remeber the current offsets
+        let init_offsets = try!(client.fetch_offsets(topics.iter().cloned().collect(), -1));
+        debug!("init_offsets: {:#?}", init_offsets);
+
+        // ~ send the messages
+        let n_msgs = msgs.len();
+        try!(client.send_messages(1, 1000, msgs));
+        debug!("Sent {} messages", n_msgs);
+
+        // ~ now fetch the messages and verify we could read them back
+        let mut reqs = vec![];
+        for topic in topics {
+            let init_offsets = init_offsets.get(topic).unwrap();
+            let partitions = client.topic_partitions.get(topic).unwrap();
+            for partition in partitions {
+                reqs.push(kafka::utils::TopicPartitionOffset {
+                    topic: topic.clone(),
+                    partition: *partition,
+                    offset: find_offset(init_offsets, *partition).unwrap(),
+                });
+            }
+        }
+
+        debug!("Fetching messages for: {:#?}", reqs);
+        let fetched_msgs = try!(client.fetch_messages_multi(reqs));
+        debug!("Fetched {} messages", fetched_msgs.len());
+        let mut msgs_per_topic = HashMap::with_capacity(topics.len());
+        for msg in fetched_msgs {
+            assert_eq!(msg.message, sent_msg);
+            if let Some(v) = msgs_per_topic.get_mut(&msg.topic) {
+                *v = *v + 1;
+                continue;
+            }
+            msgs_per_topic.insert(msg.topic.clone(), 1);
+        }
+        for (_, v) in msgs_per_topic {
+            assert_eq!(v, msg_per_topic);
+        }
+        debug!("Verified all fetched messages");
+        Ok(())
+    }
+
+    fn find_offset(offsets: &[kafka::utils::PartitionOffset], partition: i32) -> Option<i64> {
+        offsets.into_iter()
+            .filter(|off| off.partition == partition)
+            .next()
+            .and_then(|off| Some(off.offset))
+    }
+
+    // ~ --------------------------------------------------------------
+
+    if cfg.topics.is_empty() {
+        return Err(Error::Other("At least one topic must be explicitely provided!".to_owned()));
+    }
+
+    // ~ make sure all of the topics we will be sending messages to do exist
+    let mut client = try!(cfg.new_client());
+    try!(client.load_metadata_all());
+    for topic in &cfg.topics {
+        if !client.topic_partitions.contains_key(topic) {
+            return Err(Error::Other(format!("Non existent topic: {}", topic)));
+        }
+    }
+
+    // ~ our test messages
+    let s = b"hello, world";
+    let sent_msg: Vec<u8> = s.into_iter().cycle().take(cfg.produce_bytes_per_msg as usize).cloned().collect();
+    // ~ run the test
+    try!(do_test(&mut client, &cfg.topics, cfg.produce_msg_per_topic as usize, &sent_msg));
+
     Ok(())
 }
